ba55b7de4b2ef0643a79630ee5c91ccf3754c1d0
Tam Nguyen Cu <tcu@ivytech.edu>
2024-09-13 00:11:19 -0400

    Chapter 4 tutorials

---
+++ repo/SDEV200/Chapter4/Tutorials/REdemo.java
@@ -0,0 +1,10 @@
+package Chapter4.Tutorials;
+//Tam Nguyen Cu
+//Application for 4-40 p. 152
+
+public class REdemo {
+    public static void main(String[] args) {
+        RealEstateListing myListing = new RealEstateListing(2, 250000, "1212 Mansion Lane", 6700);
+        myListing.display();
+    }
+}
