045d72f1e4107d779523ef7c3f19c79c186cd459
Tam Nguyen Cu <tcu@ivytech.edu>
2024-09-13 23:12:34 -0400

    Chapter 6 tutorials

---
+++ repo/SDEV200/Chapter6/Tutorials/CustomerSurvey.java
@@ -0,0 +1,30 @@
+package Chapter6.Tutorials;
+import java.util.Scanner;
+
+public class CustomerSurvey {
+    public static void main(String[] args) {
+        int rating;
+        final int MIN = 1;
+        final int MAX = 5;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a value that");
+        System.err.println("represents your satisfaction with");
+        System.out.println("our service.");
+        System.out.println("Enter a value between " + MIN);
+        System.out.println("and " + MAX);
+        System.out.println("with " + MAX + " meaning highly");
+        System.out.println("satisfied and");
+        System.out.println(MIN + " meaning not at all satisfied.");
+        System.err.print("Enter your rating >> ");
+        rating = input.nextInt();
+
+        while (rating < MIN || rating > MAX) {
+            System.out.println("You must enter a value");
+            System.out.println("between " + MIN + " and " + MAX);
+            System.out.print("Please try again >> ");
+            rating = input.nextInt();
+        }
+
+        System.out.println("Thank you.");
+    }
+}
