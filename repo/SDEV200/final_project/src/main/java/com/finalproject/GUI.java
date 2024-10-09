4a3f62eddfd222f6c586853c5b0e527747f46b8b
Tam Nguyen Cu <tcu@ivytech.edu>
2024-10-09 03:08:51 -0400

    Update menu GUI

--- repo/SDEV200/final_project/src/main/java/com/finalproject/GUI.java
+++ repo/SDEV200/final_project/src/main/java/com/finalproject/GUI.java
@@ -36,7 +36,7 @@ public class GUI {
         
         JPanel sidePanel = new JPanel();
         sidePanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        sidePanel.setBounds(0, 49, 300, 750);
+        sidePanel.setBounds(0, 59, 300, 740);
         sidePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 250, 20));
 
         JPanel itemPanel = new JPanel();
@@ -48,18 +48,21 @@ public class GUI {
 
         JButton coffeeBtn = new JButton(COFFEE);
         coffeeBtn.setFont(new Font("Ariel", Font.BOLD, 20));
+        coffeeBtn.setPreferredSize(new Dimension(200,50));
         coffeeBtn.addActionListener(e -> {
             getMenuItem(COFFEE, itemPanel);
         });
 
         JButton teaBtn = new JButton(TEA);
         teaBtn.setFont(new Font("Ariel", Font.BOLD, 20));
+        teaBtn.setPreferredSize(new Dimension(200,50));
         teaBtn.addActionListener(e -> {
             getMenuItem(TEA, itemPanel);
         });
 
         JButton foodBtn = new JButton(FOOD);
         foodBtn.setFont(new Font("Ariel", Font.BOLD, 20));
+        foodBtn.setPreferredSize(new Dimension(200,50));
         foodBtn.addActionListener(e -> {
             getMenuItem(FOOD, itemPanel);
         });
@@ -75,23 +78,31 @@ public class GUI {
     public static void loadHeader(JFrame frame) {
         JPanel headerPanel = new JPanel();
         headerPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        headerPanel.setBounds(0, 0, 1280, 50);
+        headerPanel.setBounds(0, 0, 1280, 60);
         headerPanel.setLayout(new FlowLayout(FlowLayout.CENTER,50,10));
+        
         JLabel appName = new JLabel("My Coffee App");
         appName.setFont(new Font("Ariel", Font.BOLD, 24));
-        JButton menuButton = new JButton("Menu");
-        menuButton.addActionListener(e -> {
-            System.out.println("Page under construction!");
+
+        JButton menuBtn = new JButton("Menu");
+        menuBtn.setFont(new Font("Ariel", Font.BOLD, 20));
+        menuBtn.setPreferredSize(new Dimension(200,30));
+        menuBtn.setFocusable(false);
+        menuBtn.addActionListener(e -> {
+            loadMenuGUI(frame);
         });
 
-        JButton cartButton = new JButton("Cart");
-        cartButton.addActionListener(e -> {
+        JButton cartBtn = new JButton("Cart");
+        cartBtn.setFont(new Font("Ariel", Font.BOLD, 20));
+        cartBtn.setPreferredSize(new Dimension(200,30));
+        cartBtn.setFocusable(false);
+        cartBtn.addActionListener(e -> {
             System.out.println("Page under construction!");
         });
 
         headerPanel.add(appName);
-        headerPanel.add(menuButton);
-        headerPanel.add(cartButton);
+        headerPanel.add(menuBtn);
+        headerPanel.add(cartBtn);
         frame.add(headerPanel);
     }
 
@@ -108,11 +119,12 @@ public class GUI {
             icon = new ImageIcon(newImage);
 
             // Generate item in menu
-            JButton itemBtn = new JButton(item.getName());
+            JButton itemBtn = new JButton(item.getName() + " - $" + item.getPrice());
             itemBtn.setIcon(icon);
-            itemBtn.setHorizontalTextPosition(JButton.TOP);
+            itemBtn.setFocusable(false);
+            itemBtn.setHorizontalTextPosition(JButton.CENTER);
             itemBtn.setVerticalTextPosition(JButton.BOTTOM);
-            itemBtn.setPreferredSize(new Dimension(200,250));
+            itemBtn.setPreferredSize(new Dimension(200,235));
             panel.add(itemBtn);
         }
         panel.setVisible(true);
